567b7bff564e91b8cf918fba9dfbd5e2f1dbb7a9
Aya <ayadaaboul02@outlook.com>
2025-01-13 20:47:34 +0200

    data updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
-import Weather from './components/Weather'
+import React, { useState, useEffect } from 'react';
+import Weather from './components/Weather';
+const c = prompt("enter a city")
 const App = () => {
-  return (
-    <div className='app'>
-      <Weather/>
-    </div>
-  )
-}
+  const [weatherData, setWeatherData] = useState(null); 
+  const [loading, setLoading] = useState(true); 
+  const [error, setError] = useState(null); 
 
-export default App
+  // Fetch weather data
+  async function getWeather() {
+    try {
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=4448ba14cb38fc131a490716e8b93b25"
+      );
+      if (!response.ok) {
+        throw new Error("Unable to fetch");
+      }
+   
+      const data = await response.json();
+      
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      //in order to do search functionality we should recieve the input city and change the lon and lat accordingly
+
+      const city = data.name;
+      const temp = data.main.temp;
+      const desc = data.weather[0].description;
+      const feelslike=data.main.feels_like;
+      const tempmin=data.main.temp_min;
+      const tempmax=data.main.temp_max;
+      const pressure=data.main.pressure;
+      const humidity=data.main.humidity;
+      const windspeed=data.wind.speed;
+      
+
+
+      setWeatherData({ city, temp, desc, feelslike, tempmin,tempmax,pressure,humidity,windspeed,icon,});
+      setLoading(false);
+    } catch (err) {
+      setError(err.message); 
+      setLoading(false);
+    }
+  }
+
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+ 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="app">
+      <Weather data={weatherData} /> 
+    </div>
+  );
+};
+
+export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,17 +1,51 @@
-import React from 'react'
-import './Weather.css'
-import search_icon from '../assets/search.png'
-
-const Weather = () => {
+import React from 'react';
+import './Weather.css';
+import search_icon from '../assets/search.png';
+import bal from '../assets/temp.png'
+import wind from '../assets/image 2.png'
+import humid from '../assets/image 6.png'
+const Weather = ({ data }) => {
   return (
-    <div className="weather">
-        <div className='search-bar'>
-        <img src={search_icon} width='28px' height='30px'></img>
-            <input type='text' placeholder='Search Weather by city'></input>
-        </div>
-        <img src={search_icon}></img>
-    </div>
-  )
-}
+    <div className='weather-card'>
+      <div className='search-bar'>
+        <img src={search_icon} width='28px' height='30px' alt="Search Icon" />
+        <input type='text' placeholder='Search Weather by city' />
+      </div>
 
-export default Weather
+      <div className='blocks'>
+      <div className="weather-n-city-info">
+        <h2>{data.city}</h2>
+        <p className='temp-bold'>{data.temp} K</p>
+        <p className='desc'>{data.desc}</p>
+        <img src={data.icon} alt='weather icon' width='200px' height='200px'></img>
+        
+        <div className='humidity_pr'>
+        <img src={humid} width='20px' height='20px'></img>
+        <h3>Humidity: </h3>
+        <h4>{data.humidity}</h4>
+        <img src={wind} width='30px' height='38px'></img>
+        <h3>Wind: </h3>
+        <h4>{data.windspeed}</h4>
+      </div>
+
+      </div>
+      
+
+
+      <div className='temp-info'>
+        <img src={bal}></img>
+        <h4>Feels like: <br></br></h4>
+        <h4>{data.feelslike}</h4>
+
+        <h4>Minimum temperature <br></br></h4>
+        <h4>{data.tempmin}</h4>
+
+        <h4>Maximum Temperature <br></br></h4>
+        <h4>{data.tempmax}</h4>
+      </div> 
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
